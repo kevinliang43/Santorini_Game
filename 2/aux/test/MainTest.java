c67ab6bf0d90cd4e428600dbefee3e739eadfd90
kevinliang43 <kevinliang43@gmail.com>
2018-09-13 21:25:11 -0400

    Initial commit for Homework 2.

---
+++ 2/aux/test/MainTest.java
@@ -0,0 +1,33 @@
+/*
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ * Created by KevinLiang on 9/13/18.
+
+public class MainTest {
+
+  private final ByteArrayOutputStream outMessages = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
+
+  @Before
+  public void initializeStreams() {
+    System.setOut(new PrintStream(outMessages));
+  }
+
+  @After
+  public void resetStreams() {
+    System.setOut(originalOut);
+  }
+
+  @Test
+  public void testEmptyJson() {
+
+  }
+
+}
+
+*/
