552cb62bb668af48a858c87c9f3daec8b2f8eb60
kevinliang43 <kevinliang43@gmail.com>
2018-09-20 23:05:08 -0400

    Fixed Main function to prompt for sign up name. Included more comments.

--- 4/aux/Client/src/Main.java
+++ 4/aux/Client/src/Main.java
@@ -17,6 +17,7 @@ public class Main {
 
     // Initialize Client
     Client client = new Client();
+    System.out.println("Connecting to Server... ");
 
     // Connect Client
     while (!client.isConnected()) {
@@ -25,49 +26,51 @@ public class Main {
         System.out.println("Successful Connection to: " + args[0]);
         System.out.println("Connection at port: " + PORT_NUMBER);
       } catch (IOException e) {
-        e.printStackTrace();
+
       }
     }
 
-//    // Get name from STDIN
-//    // Name is valid if the string length is > 2
-//    Scanner scanner = new Scanner(System.in);
-//    String signupName = "";
-//    while (signupName.length() < 2) {
-//      System.out.println("Please enter a Valid Name: ");
-//      signupName = scanner.nextLine();
-//    }
-//
-//
-//
-//
-//    // Signup Name
-//    try {
-//      client.signupName(signupName);
-//    } catch (IOException e) {
-//      System.err.println("Unable to sign up name. Please check your connection.");
-//      e.printStackTrace();
-//    }
-//    String name = "";
-//
-//
-//    // Receive Name to check for successful signup
-//    try {
-//      name = client.recieveName();
-//    } catch (IOException e) {
-//      System.err.println("Unable to Receive Name from server. Please check your connection.");
-//      e.printStackTrace();
-//    }
-//
-//    System.out.println(name);
+    // Get name from STDIN
+    // Name is valid if the string length is > 2
+    Scanner scanner = new Scanner(System.in);
+    String signupName = "";
+    while (signupName.length() < 2) {
+      System.out.println("Please enter a Valid Name: ");
+      signupName = scanner.nextLine();
+    }
+
+
+    // Signup Name
+    try {
+      client.signupName(signupName);
+    } catch (IOException e) {
+      System.err.println("Unable to sign up name. Please check your connection.");
+      e.printStackTrace();
+    }
+    String name = "";
+
+
+    // Receive Name to check for successful signup
+    try {
+      name = client.recieveName();
+    } catch (IOException e) {
+      System.err.println("Unable to Receive Name from server. Please check your connection.");
+      e.printStackTrace();
+    }
+
+    System.out.println(name);
 
 
     while (true) {
       try {
-
+        // Prompt for Request
         System.out.println("Please enter requests: ");
 
+        // Get Request Inputs
         ArrayList<String> requests = client.takeInput();
+
+        // Send Batch and print out Server Response
+        // -666 for invalid JSON
         System.out.println(client.sendBatch(requests));
 
       } catch (IOException e) {
